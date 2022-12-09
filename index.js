e97d9f4d63f93198120211a8a498cd1577446772
Abhijit-Nag <100556261+Abhijit-Nag@users.noreply.github.com>
2022-12-10 00:27:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const cors=require("cors");
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname,"/images")));
 app.use(cors({
-    origin:["https://abhijit-mern-blog-app.netlify.app","http://localhost:3000"]
+    origin:["https://abhijit-mern-blog-app.netlify.app","http://localhost:3000"],
 }));
 
 const authRoute=require("./routes/auth");
