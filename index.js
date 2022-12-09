eb2a2fd46a623734089259cebd11d94f0befa0e6
Abhijit-Nag <100556261+Abhijit-Nag@users.noreply.github.com>
2022-12-10 00:58:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const cors=require("cors");
 // json format 
 app.use(express.json());
 app.use("/images", express.static(path.join(__dirname,"/images")));
-//app.use(cors({
-//    origin:["https://abhijit-mern-blog-app.netlify.app","http://localhost:3000"],
-//}));
+app.use(cors({
+    origin:["https://abhijit-mern-blog-app.netlify.app","http://localhost:3000"],
+}));
 
-app.options('*',cors());
+//app.options('*',cors());
 
 var headerSettings = function(req, res, next) {
     res.header('Access-Control-Allow-Origin', 'example.com');
