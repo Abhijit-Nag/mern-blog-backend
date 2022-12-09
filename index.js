3ccf74f9d513933facf1748fee71a49ae5a95226
Abhijit-Nag <100556261+Abhijit-Nag@users.noreply.github.com>
2022-12-10 00:54:36 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,9 +17,15 @@ app.use("/images", express.static(path.join(__dirname,"/images")));
 
 app.options('*',cors());
 
-header('Access-Control-Allow-Origin: *');
-header('Access-Control-Allow-Methods: POST, GET, OPTIONS, PUT, DELETE');
-header('Access-Control-Allow-Headers: Content-Type, X-Auth-Token, Origin, Authorization');
+var headerSettings = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(headerSettings);
 
 const authRoute=require("./routes/auth");
 const usersRoute= require("./routes/users");
